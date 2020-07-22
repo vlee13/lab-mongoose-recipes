49d9494b464f339a6424ea684c788c85e48e6f88
vlee13 <vlee13@gmail.com>
2020-07-22 13:41:23 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,27 +1,94 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI =
+  "mongodb+srv://val:pnv050212@cluster0.mgb3j.mongodb.net/Recipes?retryWrites=true&w=majority";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Chicken Curry",
+      cuisine: "Thai",
+    }).then((res) => {
+      console.log(res);
+    });
+
+    Recipe.insertMany(data).then((res) => {
+      res.map((eachRec) => {
+        console.log(eachRec.title);
+      });
+      Recipe.findOneAndUpdate(
+        { title: "Rigatoni alla Genovese" },
+        { $set: { duration: 100 } },
+        { new: true } //updates nodemon recipe?
+      ).then((res) => {
+        console.log("Updated Rigatoni!!!");
+      });
+      Recipe.deleteOne({ title: "Carrot Cake" })
+        .then((res) => {
+          console.log("No more Carrot Cake!!!");
+        })
+        .then(() => {
+          mongoose.connection.close();
+        });
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+//   .then(async () => {
+//     let res = await Recipe.create({title:'efwef'})
+//     console.log(res.title)
+//     res = await Recipe.insertMany(data)
+//     res.forEach((eachRecipe)=>{
+//        console.log(eachRecipe.title)
+//      })
+//     res = await Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration:100}, {new:true})
+//       console.log('Rigatoni duration updated to 100');
+//     res = await Recipe.deleteOne({title: "Carrot Cake"},(err)=>{
+//       if (err)
+//         console.log('error')
+//       console.log('Item successfully deleted')
+//   })
+// })
+
+// .then(() => {
+//   Recipe.create({
+//     title: "Chicken Curry",
+//     cuisine: "Thai",
+//   }).then((res) => {
+//     console.log(res);
+//   });
+
+//   Recipe.insertMany(data).then((res) => {
+//     res.map((eachRec) => {
+//       console.log(eachRec.title);
+//     });
+//     Recipe.findOneAndUpdate(
+//       { title: "Rigatoni alla Genovese" },
+//       { $set: { duration: 100 } },
+//       { new: true } //updates nodemon recipe?
+//     ).then((res) => {
+//       console.log("Updated Rigatoni!!!");
+//     });
+//     Recipe.deleteOne({ title: "Carrot Cake" }).then((res) => {
+//       console.log("No more Carrot Cake!!!");
+//     });
+//   });
+// })
